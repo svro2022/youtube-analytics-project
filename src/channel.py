60007a6a3f03c2a76ff9397bb0bf2a36e50456dc
svro <ichistrawberry2012@gmail.com>
2023-05-05 15:32:42 +0300

    change channel.py again

--- src/channel.py
+++ src/channel.py
@@ -6,10 +6,9 @@ from googleapiclient.discovery import build
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('API_KEY')
-# API_KEY = 'AIzaSyCQteHpCPaNYLR27mPcVmOXGQHKSimEUFU'
 
-# создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey=api_key)
+
+# API_KEY = 'AIzaSyCQteHpCPaNYLR27mPcVmOXGQHKSimEUFU'
 
 
 class Channel:
@@ -17,6 +16,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        # создать специальный объект для работы с API
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
         self.channel_id = channel_id
         self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
