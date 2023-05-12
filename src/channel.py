c71f91c66fe80141b304f4034defe240980bdb2c
svro <ichistrawberry2012@gmail.com>
2023-05-12 08:43:49 +0300

    change channel.py for h3

--- src/channel.py
+++ src/channel.py
@@ -25,17 +25,50 @@ class Channel:
         self.video_count = self.channel['items'][0]['statistics']['videoCount']  # количество видео
         self.viewCount = self.channel['items'][0]['statistics']['viewCount']  # общее количество просмотров
 
+    def __str__(self):
+        """Магический метод __str__ для отображения информации об объекте класса для пользователей"""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Магический метод __add__ сложения подписчиков"""
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        """Магический метод __sub__ вычитания подписчиков"""
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __gt__(self, other):
+        """Магический метод __gt__ сравнения (>) подписчиков"""
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        """Магический метод __ge__ сравнения (>=) подписчиков"""
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __lt__(self, other):
+        """Магический метод __lt__ сравнения (<) подписчиков"""
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        """Магический метод __le__ сравнения (<=) подписчиков"""
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        """Магический метод __eq__ сравнения (==) подписчиков"""
+        return int(self.subscriberCount) == int(other.subscriberCount)
+
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
     @classmethod
     def get_service(cls):
-        """класс-метод `get_service()`, возвращающий объект для работы с YouTube API"""
+        """Класс-метод `get_service()`, возвращающий объект для работы с YouTube API"""
         return cls.youtube
 
     def to_json(self, filename):
-        """метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра Channel"""
+        """Метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра Channel"""
 
         data = {'channel_id': self.channel_id,
                 'channel': self.channel,
