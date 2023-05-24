d8f5438051638e0477e73ba04e9be4cb093b9131
svro <ichistrawberry2012@gmail.com>
2023-05-24 17:24:27 +0300

    changes for h6

--- src/video.py
+++ src/video.py
@@ -1,6 +1,6 @@
 import os
-import json
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -15,12 +15,27 @@ class Video:
     def __init__(self, video_id) -> None:
         """Экземпляр инициализируется video_id. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id  # id видео
-        self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-
-        self.video_title: str = self.video_info['items'][0]['snippet']['title']  # название видео
-        self.video_url = self.video_info['items'][0]['snippet']['thumbnails']['default']['url']  # ссылка на видео
-        self.video_number_views: int = self.video_info['items'][0]['statistics']['viewCount']  # количество просмотров
-        self.video_number_like: int = self.video_info['items'][0]['statistics']['likeCount']  # количество лайков
+        self.title = None
+        self.url = None
+        self.views_count = None
+        self.like_count = None
+        try:
+            self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            if self.video_info['items']:
+                self.video_title: str = self.video_info['items'][0]['snippet']['title']  # название видео
+                self.video_url = self.video_info['items'][0]['snippet']['thumbnails']['default'][
+                    'url']  # ссылка на видео
+                self.video_number_views: int = self.video_info['items'][0]['statistics'][
+                    'viewCount']  # количество просмотров
+                self.video_number_like: int = self.video_info['items'][0]['statistics'][
+                    'likeCount']  # количество лайков
+            else:
+                print(f"Видео {self.video_id} не найдено")
+        except HttpError as e:
+            if e.resp.status == 404:
+                print(f"Видео {self.video_id} не найдено")
+            else:
+                print(f"Ошибка при получении информации о видео {self.video_id}: {e}")
 
     def __str__(self):
         return self.video_title
